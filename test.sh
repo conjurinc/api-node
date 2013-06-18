bd00eb28fc19054e59e7cfecf93f016e4e7f7924
Kevin Gilpin <kgilpin@gmail.com>
2013-06-18 15:06:22 -0400

    use winston to log; setup winston for testing; add test script

---
+++ test.sh
@@ -0,0 +1,1 @@
+mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script test/ > test-result.xml
---
+++ testconfig.js
@@ -0,0 +1,5 @@
+var winston = require('winston')
+  ;
+
+winston.remove(winston.transports.Console);
+winston.add(winston.transports.File, {filename: 'test.log'})
