2253b461a86a00d02b6e2055962b47995b567962
Kevin Gilpin <kgilpin@gmail.com>
2013-07-15 10:01:18 -0400

    automatically generate test assets if necessary

--- integration/generate_assets.sh
+++ integration/generate_assets.sh
@@ -3,14 +3,13 @@ set -e
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
-rvm use 1.9.3@conjur-cli
+rvm use 1.9.3@conjur-cli-ci
 
 # Bootstrap
 WORKDIR=$1
-CONJURDIR=$2
 
 if [ -z "$WORKDIR" ] ; then
-    echo "Usage: $0 <target_directory> <conjurcli_dir>"
+    echo "Usage: $0 <target_directory>"
     exit 1
 else
     echo "Generating new assets, info stored in $WORKDIR"
@@ -19,15 +18,11 @@ else
     fi
 fi
 
-pushd $WORKDIR
-dir=`pwd`
-pushd $CONJURDIR
+cd $WORKDIR
 
 # Preliminary setup
-ns=`conjur id:create | tee $dir/ns`
+ns=`conjur id:create | tee ns`
 
-CONJURAPI_LOG=stderr conjur user:create $ns-admin --no-password > $dir/admin.json
+CONJURAPI_LOG=stderr conjur user:create $ns-admin --no-password > admin.json
 CONJURAPI_LOG=stderr conjur group:create $ns/admin >/dev/null
-
 CONJURAPI_LOG=stderr conjur group:members:add $ns/admin user:$ns-admin
-
--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
+if [ ! -d integration/testdata ]; then
+  echo "Generating test assets"
+  ./integration/generate_assets.sh integration/testdata
+fi
 mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script test/ > test-result.xml
 mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script integration/ > integration-result.xml
