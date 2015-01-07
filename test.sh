4b94f83032927a9c45a48e904782fa0ada386d35
Kevin Gilpin <kgilpin@gmail.com>
2015-01-07 16:06:38 -0500

    deactivate integration tests for now

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./node_modules/mocha/bin/mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script/register test/ > test-result.xml
-./node_modules/mocha/bin/mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script/register integration/ > integration-result.xml
+# ./node_modules/mocha/bin/mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script/register integration/ > integration-result.xml
