a8bbf45d2fc3ea9837773e8f51be65a4d181905f
Jon Mason <jonathan.j.mason@gmail.com>
2015-01-13 11:32:30 -0600

    add support for non xunit output

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-./node_modules/mocha/bin/mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script/register test/ > test-result.xml
+if [ $TEST_DEV ] ; then
+    ./node_modules/mocha/bin/mocha --require testconfig --recursive --compilers coffee:coffee-script/register test/
+else
+    ./node_modules/mocha/bin/mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script/register test/ > test-result.xml
 # ./node_modules/mocha/bin/mocha --require testconfig --recursive -R xunit --compilers coffee:coffee-script/register integration/ > integration-result.xml
+fi
